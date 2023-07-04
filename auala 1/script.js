1c4d7e7e3d73776435cfdc3bed587ce71da18d9b
l.bmachadodeassi <leandro277bueno@gmail.com>
2023-07-04 18:44:28 -0300

    recursos nativos

---
+++ auala 1/script.js
@@ -0,0 +1,15 @@
+// cometrio em javascript de uma linha
+/* cometorio de mais que que linha*/
+// recursos  nativos
+
+//console 
+console. log('boa noite pessoal')
+console.info('console de informação')
+console.warn('cuidado')
+console.error('aqui temos uma mensagem de erro.')
+
+//Caixas de diálogo:
+
+window.alert('Janela de alerta!')//janela de alerta
+window.confirm('Tudo bem pessoal?')//Janela de confirmação
+window.prompt('Qual seu nome?')//Janela para obter algum dado 
